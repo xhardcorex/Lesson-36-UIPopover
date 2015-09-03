48fb6af28fe0a371ef667cbe5d780f5644b39a10
xhardcorex <nik@MacBook-Air-Nik.local>
2015-09-03 22:24:02 +0300

    Initial Commit

---
+++ Lesson 36 UIPopover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson 36 UIPopover
+//
+//  Created by Nik on 03.09.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson 36 UIPopover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson 36 UIPopover
+//
+//  Created by Nik on 03.09.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
